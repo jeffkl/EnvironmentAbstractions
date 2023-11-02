4d1527171d7cd0d20b0642e81a0b830b7e998ad0
Jeff Kluge <jeffkl@microsoft.com>
2023-11-02 14:08:22 -0700

    Update packages, drop support for .NET Core 3, add .NET 8

--- src/EnvironmentAbstractions.UnitTests/HashtableIDictionaryWrapperTests.cs
+++ src/EnvironmentAbstractions.UnitTests/HashtableIDictionaryWrapperTests.cs
@@ -31,18 +31,6 @@ namespace EnvironmentAbstractions.UnitTests
         }
 
         [Fact]
-        public void GetEnvironmentVariablesWrapperCountSameAsGetEnvironmentVariables()
-        {
-            GetEnvironmentVariablesWrapper actual = new GetEnvironmentVariablesWrapper();
-
-            IDictionary expected = Environment.GetEnvironmentVariables();
-
-            actual.ToDictionary(i => i.Key, i => i.Value).ShouldBe(expected.Cast<DictionaryEntry>().ToDictionary(i => (string)i.Key, i => (string)i.Value!));
-
-            actual.Count.ShouldBe(expected.Count);
-        }
-
-        [Fact]
         public void GetEnvironmentVariablesWrapperEnumerateSeveralTimes()
         {
             GetEnvironmentVariablesWrapper wrapper = new GetEnvironmentVariablesWrapper();
@@ -112,7 +100,7 @@ namespace EnvironmentAbstractions.UnitTests
 
             foreach (KeyValuePair<string, string> environmentVariable in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().Select(i => new KeyValuePair<string, string>((string)i.Key, (string)i.Value!)))
             {
-                wrapper.TryGetValue(environmentVariable.Key, out string actual).ShouldBeTrue();
+                wrapper.TryGetValue(environmentVariable.Key, out string actual).ShouldBeTrue($"{environmentVariable.Key} should exist");
 
                 actual.ShouldBe(environmentVariable.Value);
             }
